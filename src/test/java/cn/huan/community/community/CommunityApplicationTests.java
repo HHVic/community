e177abe9915f24004fa14d4a74b1b0bfdfe69963
HHVic <182070022@stu.just.edu.cn>
2020-07-16 13:04:32 +0800

    init repo

---
+++ src/test/java/cn/huan/community/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.huan.community.community;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
