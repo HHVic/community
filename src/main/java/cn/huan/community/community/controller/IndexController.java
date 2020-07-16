a9511483bd7e285c5a48117f718281469b1bc422
HHVic <182070022@stu.just.edu.cn>
2020-07-16 13:37:12 +0800

    add index and bootstrap

---
+++ src/main/java/cn/huan/community/community/controller/IndexController.java
@@ -0,0 +1,12 @@
+package cn.huan.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String hello(){
+        return "index";
+    }
+}
