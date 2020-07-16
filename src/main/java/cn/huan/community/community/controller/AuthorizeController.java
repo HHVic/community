14b29e4a300ad0337577e507b9dafafff1bf2e66
HHVic <182070022@stu.just.edu.cn>
2020-07-16 17:38:15 +0800

    extract github config from code to properties

--- src/main/java/cn/huan/community/community/controller/AuthorizeController.java
+++ src/main/java/cn/huan/community/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import cn.huan.community.community.dto.AccessToken;
 import cn.huan.community.community.dto.GithubUser;
 import cn.huan.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +13,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${Github.client.id}")
+    private String client_id;
+    @Value("${Github.client.secret}")
+    private String client_secret;
+    @Value("${Github.redirect.uri}")
+    private String redirect_uri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessToken accessToken = new AccessToken();
-        String client_id = "689c94ad0ea8ba8267bb";
-        String client_secret = "4079a093ad86a54ceca9e6dd0820a620bfad5641";
         accessToken.setCode(code);
-        String redirect_uri = "http://huan.cross.echosite.cn/callback";
         accessToken.setRedirect_uri(redirect_uri);
         accessToken.setState(state);
         accessToken.setClient_id(client_id);
